f1271ac7f05e336e27092622cbc0c658b1cbc12c
perlancar (@pc-office) <perlancar@gmail.com>
2015-07-10 16:04:01 +0700

    Initial commit

---
+++ lib/App/CheckModuleVersion.pm
@@ -0,0 +1,11 @@
+package App::CheckModuleVersion;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Check module (e.g. check latest version) with CPAN (or equivalent repo)
+
+=head1 DESCRIPTION
+
+This distribution contains L<check-module-version> utility.
